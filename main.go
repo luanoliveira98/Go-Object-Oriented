63e9460a2ee87d0fbfee6f7595eb371423a8eea6
Luan Oliveira <luanoliveira17@hotmail.com>
2020-09-23 11:01:32 -0300

    Variables and types

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var holder string = "Luan"
+	var numberAgency int = 589
+	var numberAccount int = 123456
+	var ballance float64 = 125.5
+
+	fmt.Println(holder, numberAgency, numberAccount, ballance)
+
+	var holder2 string = "Luan"
+	var numberAgency2 int = 589
+	var numberAccount2 int = 123456
+	var ballance2 float64 = 125.5
+
+	fmt.Println(holder2, numberAgency2, numberAccount2, ballance2)
+}
