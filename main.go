88d87b848d60bb2f642e4c38cb9e973ede36d557
Luan Oliveira <luanoliveira17@hotmail.com>
2020-09-23 11:27:34 -0300

    Structs

--- main.go
+++ main.go
@@ -2,18 +2,13 @@ package main
 
 import "fmt"
 
+type CheckingAccount struct {
+	holder        string
+	numberAgency  int
+	numberAccount int
+	ballance      float64
+}
+
 func main() {
-	var holder string = "Luan"
-	var numberAgency int = 589
-	var numberAccount int = 123456
-	var ballance float64 = 125.5
-
-	fmt.Println(holder, numberAgency, numberAccount, ballance)
-
-	var holder2 string = "Luan"
-	var numberAgency2 int = 589
-	var numberAccount2 int = 123456
-	var ballance2 float64 = 125.5
-
-	fmt.Println(holder2, numberAgency2, numberAccount2, ballance2)
+	fmt.Println(CheckingAccount{})
 }
