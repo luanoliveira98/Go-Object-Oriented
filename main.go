4c8b4cc6e3f6cd7440a396816b85a0b78573e9b5
Luan Oliveira <luanoliveira17@hotmail.com>
2020-09-24 11:47:27 -0300

    Tipos aninhados

--- main.go
+++ main.go
@@ -3,16 +3,13 @@ package main
 import (
 	"fmt"
 
+	"./clientes"
 	"./contas"
 )
 
 func main() {
-	contaDaEstefane := contas.ContaCorrente{Titular: "Estefâne", Saldo: 300}
-	contaDoLuan := contas.ContaCorrente{Titular: "Luan", Saldo: 100}
+	clienteBruno := clientes.Titular{"Bruno", "123.111.123.12", "Desenvolvedor GO"}
+	contaDoBruno := contas.ContaCorrente{clienteBruno, 123, 123456, 100}
 
-	status := contaDaEstefane.Transferir(200, &contaDoLuan)
-
-	fmt.Println(status)
-	fmt.Println(contaDaEstefane)
-	fmt.Println(contaDoLuan)
+	fmt.Println(contaDoBruno)
 }
