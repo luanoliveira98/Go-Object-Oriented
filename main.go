59399e2b38142975e49df0d4bd8d3c5e2a55c29a
Luan Oliveira <luanoliveira17@hotmail.com>
2020-09-24 10:51:00 -0300

    Multiplos retornos

--- main.go
+++ main.go
@@ -18,13 +18,20 @@ func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
 	return "Saldo insuficiente!"
 }
 
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "Deposito realizado com sucesso!", c.saldo
+	}
+	return "Valor do deposito menor que zero!", c.saldo
+}
+
 func main() {
 	contaDaEstefane := ContaCorrente{}
 	contaDaEstefane.titular = "Estefâne"
 	contaDaEstefane.saldo = 500
 
 	fmt.Println(contaDaEstefane.saldo)
-
-	fmt.Println(contaDaEstefane.Sacar(-100))
-	fmt.Println(contaDaEstefane.saldo)
+	status, valor := contaDaEstefane.Depositar(1000)
+	fmt.Println(status, valor)
 }
